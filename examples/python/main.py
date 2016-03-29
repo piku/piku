03a7496f4552607511758b9a3ae044582b98a434
Rui Carmo <rui.carmo@gmail.com>
2016-03-29 21:41:03 +0100

    Tweaks

--- examples/python/main.py
+++ examples/python/main.py
@@ -1,10 +1,14 @@
 import os
-from bottle import app, route
+from bottle import app, get
 
-@route("/")
+@get("/")
 def default():
     table = ['<table border="0">']
     for k, v in os.environ.iteritems():
         table.append('<tr><th>%s</th><td>%s</td></tr>' % (k, v))
     table.append('</table>')
     return '\n'.join(table)
+
+
+if __name__ == '__main__':
+    run(port=int(os.environ.get("PORT",8080)))
