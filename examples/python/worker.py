2565bcfc96c7590c27aa6c1a492fcb5956a724c6
Rui Carmo <rcarmo@users.noreply.github.com>
2018-06-14 22:19:40 +0100

    Python 3

--- examples/python/worker.py
+++ examples/python/worker.py
@@ -1,6 +1,6 @@
 import os, sys, time
 
-for x in xrange(0,int(os.environ['RANGE'])):
-    print x
+for x in range(0,int(os.environ['RANGE'])):
+    print(x)
     sys.stdout.flush()
     time.sleep(int(os.environ['INTERVAL']))
