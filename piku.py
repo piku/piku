57b60ca73542f7a6776e177506e74d1bd426f584
Chris McCormick <chris@mccormick.cx>
2019-11-27 21:51:10 +0800

    Reordering imports for cleaner merge.

--- piku.py
+++ piku.py
@@ -12,10 +12,10 @@ from importlib import import_module
 from collections import defaultdict, deque
 from fcntl import fcntl, F_SETFL, F_GETFL
 from glob import glob
+from os import chmod, getgid, getuid, symlink, unlink, remove, stat, listdir, environ, makedirs, O_NONBLOCK
+from os.path import abspath, basename, dirname, exists, getmtime, join, realpath, splitext
 from grp import getgrgid
 from pwd import getpwuid
-from os.path import abspath, basename, dirname, exists, getmtime, join, realpath, splitext
-from os import chmod, getgid, getuid, symlink, unlink, remove, stat, listdir, environ, makedirs, O_NONBLOCK
 from re import sub
 from shutil import copyfile, rmtree, which
 from socket import socket, AF_INET, SOCK_STREAM
