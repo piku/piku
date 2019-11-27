2133a4e6a9a10be950af6ecd780ce3c725f6b9fc
Rui Carmo <rcarmo@users.noreply.github.com>
2019-11-27 16:05:52 +0000

    Fix #150 and #154

--- piku.py
+++ piku.py
@@ -13,9 +13,10 @@ from fcntl import fcntl, F_SETFL, F_GETFL
 from glob import glob
 from json import loads
 from multiprocessing import cpu_count
-from os import chmod, getgid, getuid, getgrgid, symlink, unlink, remove, stat, listdir, environ, makedirs, O_NONBLOCK
+from os import chmod, getgid, getuid, symlink, unlink, remove, stat, listdir, environ, makedirs, O_NONBLOCK
 from os.path import abspath, basename, dirname, exists, getmtime, join, realpath, splitext
 from pwd import getpwuid
+from grp import getgrgid
 from re import sub
 from shutil import copyfile, rmtree, which
 from socket import socket, AF_INET, SOCK_STREAM
