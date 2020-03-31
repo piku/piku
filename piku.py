f2e861cc0be19106efc54963ab3e16ba860a0ce9
Rui Carmo <rcarmo@users.noreply.github.com>
2020-03-31 18:59:29 +0100

    Enforce server_name in return header

--- piku.py
+++ piku.py
@@ -139,7 +139,7 @@ NGINX_PORTMAP_FRAGMENT = """
     proxy_http_version 1.1;
     proxy_set_header Upgrade $http_upgrade;
     proxy_set_header Connection "upgrade";
-    proxy_set_header Host $http_host;
+    proxy_set_header Host $host;
     proxy_set_header X-Forwarded-Proto $scheme;
     proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
     proxy_set_header X-Remote-Address $remote_addr;
