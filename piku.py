c455991f0ea4f6b0f240b35e17414b29f7a23788
Rui Carmo <rui.carmo@gmail.com>
2023-02-02 23:11:38 +0000

    improve cron parsing

--- piku.py
+++ piku.py
@@ -284,10 +284,11 @@ def parse_procfile(filename):
                 # Check for cron patterns
                 if kind == "cron":
                     limits = [59, 24, 31, 12, 7]
-                    matches = match(CRON_REGEXP, command).groups()
-                    if matches:
+                    res = match(CRON_REGEXP, command)
+                    if res:
+                        matches = res.groups()
                         for i in range(len(limits)):
-                            if int(matches[i + 1].replace("*/", "").replace("*", "1")) > limits[i]:
+                            if int(matches[i].replace("*/", "").replace("*", "1")) > limits[i]:
                                 raise ValueError
                 workers[kind] = command
             except Exception:
